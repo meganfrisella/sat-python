074f5165a3353d9c347d04cbcbe2e6017175e1be
Megan Frisella <mmfrisella@wpi.edu>
2021-03-12 15:05:46 -0500

    debugging

--- solver.py
+++ solver.py
@@ -66,16 +66,16 @@ def printOutput(assignment):
     isSat = (assignment is not None)
     if isSat:
         for var in assignment:
-            assert(var != -1)
+            assert(assignment[var] != -1)
             # converted `var` to bool (represented as ints in our assignment)
-            result += " " + ("" if assignment[bool(var)] else "-") + str(var)
+            result += " " + ("" if bool(assignment[var]) else "-") + str(var)
 
     print(f"s {'SATISFIABLE' if isSat else 'UNSATISFIABLE'}")
     if isSat:
         print(f"v{result} 0")
 
 
-def unitClauseElim(varAssignment: dict[Literal, int], formula: list[Clause]):
+def unitClauseElim(varAssignment, formula):
     # for each unit clause {+/-x} in formula
     # 	remove all non-unit clauses containing +/-x
     # 	remove all instances of -/+x in every clause // flipped sign!
@@ -84,38 +84,42 @@ def unitClauseElim(varAssignment: dict[Literal, int], formula: list[Clause]):
         if (len(clause.literalSet) == 1):
             literal = clause.literalSet[0]
             opliteral = Literal(literal.name, not(literal.sign))
-            formula = list(filter(lambda c: not((len(c.literalSet) != 1) and literal in c.literalSet), formula))
-            formula = list(map(lambda c: filter(lambda l: not(opliteral == l), c.literalSet), formula))
-            varAssignment[literal] = int(literal.sign)
+            formula = list(filter(lambda c: not(literal in c.literalSet), formula))
+            formula = list(map(lambda c: Clause(c.id, list(filter(lambda l: not(opliteral == l), c.literalSet))), formula))
+            varAssignment[literal.name] = int(literal.sign)
     return varAssignment, formula
 
 
-def pureLiteralElim(varAssignment: dict[Literal, int], formula: list[Clause]):
+def pureLiteralElim(varAssignment, formula):
     # for each variable x
     #     if +/-x is pure in formula
     #         remove all clauses containing +/-x
     #         assign x consistent with its sign
+    if len(formula) == 0:
+        return varAssignment, formula
+
     sign_dict = dict()
     var_set = set(k for k in varAssignment.keys() if varAssignment[k] == -1)
 
     for clause in formula:
         literals = clause.literalSet
         for lit in literals:
-            if lit in sign_dict:
-                if sign_dict[lit] != lit.sign:
-                    var_set.remove(lit)
+            if lit.name in sign_dict:
+                if sign_dict[lit.name] != lit.sign:
+                    var_set.remove(lit.name)
             else:
-                sign_dict[lit] = lit.sign
+                sign_dict[lit.name] = lit.sign
 
     # TODO: are these similar loops to simplify?
-    formula = list(map(lambda c: filter(lambda l: not(l in var_set), c.literalSet), formula))
-    for lit in var_set:
-        varAssignment[lit] = int(lit.sign)
+    # Clause(c.id, list(filter(lambda l: not(l.name in var_set), c.literalSet)))
+    formula = list(filter(lambda c: all(list(lit.name not in var_set for lit in c.literalSet)), formula))
+    for name in var_set:
+        varAssignment[name] = int(sign_dict[name])
 
     return varAssignment, formula
 
 
-def solve(varAssignment: dict[Literal, int], formula: list[Clause]):
+def solve(varAssignment, formula):
 	# // do unit clause elim and pure literal elim on the formula
 	# unitClauseElim(formula)
 	# pureLiteralElim(formula)
@@ -131,10 +135,20 @@ def solve(varAssignment: dict[Literal, int], formula: list[Clause]):
 	# else
 	# 	return solve(varAssignment + {-x}, formula)
 
+    print(f"BEGINNING: {varAssignment.items()}, {formula}")
+
     varAssignment, formula = unitClauseElim(varAssignment, formula)
+
+    print(f"AFTER UNIT ELIM: {varAssignment.items()}, {formula}")
+
     varAssignment, formula = pureLiteralElim(varAssignment, formula)
 
+    print(f"AFTER PURE ELIM: {varAssignment.items()}, {formula}")
+
     if len(formula) == 0:
+        for name in varAssignment.keys():
+            if varAssignment[name] == -1:
+                varAssignment[name] = 1
         return varAssignment
 
     for clause in formula:
@@ -151,19 +165,21 @@ def solve(varAssignment: dict[Literal, int], formula: list[Clause]):
                 lit2freq[lit.name] = 1
     # find the most frequent variable
     sort_keys = list(k for k, v in sorted(lit2freq.items(), key=lambda item: item[1]))
-    most_freq = sort_keys[-1]
+    most_freq = sort_keys[0]
+
+    print(f"SORTED UNASSIGNED KEYS: {sort_keys}\n")
 
     # create a new unit clause with this variable (positive)
     new_id = max(list(map(lambda c: c.id, formula))) + 1
     new_literal = Literal(most_freq, True)
-    new_assigment = solve(varAssignment, Clause(new_id, [new_literal]) + formula)
+    new_assigment = solve(varAssignment, [Clause(new_id, [new_literal])] + formula)
 
     if not(new_assigment is None):
         return new_assigment
     else:
         # create a new unit clause with this variable (negative)
         new_literal = Literal(most_freq, False)
-        new_assigment = solve(varAssignment, Clause(new_id, [new_literal]) + formula)
+        new_assigment = solve(varAssignment, [Clause(new_id, [new_literal])] + formula)
         return new_assigment
 
 if __name__ == "__main__":
@@ -171,7 +187,7 @@ if __name__ == "__main__":
     varset, clauseSet = readInput(inputFile)
 
     # dictionary mapping variable name to int (-1=unassigned, 0=False, 1=True)
-    varAssignment = dict.fromKeys(varset, -1)
+    varAssignment = dict.fromkeys(varset, -1)
     varAssignment = solve(varAssignment, clauseSet)
 
     printOutput(varAssignment)
