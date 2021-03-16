3d60f098d7546bbae4472d65deb27820fce6ce91
bayazitdeniz <bayazitdeniz@gmail.com>
2021-03-15 20:35:38 -0400

    adding eval func to solve

--- test_solve.py
+++ test_solve.py
@@ -2,18 +2,60 @@ import unittest
 from solver import (
     Literal,
     Clause,
-    solve
+    solve,
+    readInput
 )
+# case 0: no clauses or empty clause?
+
+# case 1: one literal one clause formula
+
+# case 2: one literal two clause formulas
+    # sign same, sign different -> SAT, UNSAT
+# case 3:
+    # a, a b, -a c -> unit elim from final a
+
+# case 4:
+    #
+
+def evaluate(filename):
+    print("im here")
+
+    # is it sat or not
+    varset, clauseSet = readInput(filename)
+    varAssignment = dict.fromkeys(varset, -1)
+    varAssignment = solve(varAssignment, clauseSet)
+
+    # NOTE: there isn't really a way to check this
+    if varAssignment is None:
+        return False
+
+    for c in clauseSet:
+        clause = []
+        for lit in c.literalSet:
+            if lit.sign:
+                clause.append(bool(varAssignment[lit.name]))
+            else:
+                clause.append(not(bool(varAssignment[lit.name])))
+        if not(any(clause)):
+            return False
+
+    return True
 
 class SolveTests(unittest.TestCase):
 
-    def test_format(self):
-        in_varAssignment = None
-        in_formula = None
-        ##
-        out_varAssignment = None
-        ##
-        self.assertEqual(solve(in_varAssignment, in_formula), out_varAssignment)
+    def test_some_none_clause_False(self):
+        self.assertFalse(evaluate("test_files/noClause.txt"))
+        self.assertFalse(evaluate("test_files/twoClause2.txt"))
+
+    def test_some_clause_True(self):
+        self.assertTrue(evaluate("test_files/oneClause.txt"))
+        self.assertTrue(evaluate("test_files/twoClause1.txt"))
+        self.assertTrue(evaluate("test_files/twoClause3.txt"))
+
+    def test_many_clauses(self):
+        self.assertTrue(evaluate("test_files/manyClause1.txt"))
+        self.assertTrue(evaluate("test_files/manyClause2.txt"))
+        self.assertTrue(evaluate("test_files/manyClause3.txt"))
 
 if __name__ == '__main__':
     unittest.main()
