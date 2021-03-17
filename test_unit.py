3e74dcd07ce9a849c46116738aa559ce666bb0e6
bayazitdeniz <bayazitdeniz@gmail.com>
2021-03-17 17:55:06 -0400

    adding unit tests

--- test_unit.py
+++ test_unit.py
@@ -2,8 +2,10 @@ import unittest
 from solver import (
     Literal,
     Clause,
+    readInput,
     unitClauseElim
 )
+
 """
 noClause => UNSAT
 oneClause => SAT
@@ -12,19 +14,44 @@ twoClause2 => UNSAT
 manyClause1 => new formula doesn't contain var 1 or 2
 manyClause3 => formula doesn't change
 """
+
+def getInputs(filename):
+    varset, clauseSet = readInput(filename)
+    varAssignment = dict.fromkeys(varset, -1)
+    return varAssignment, clauseSet
+
 class UnitClauseElimTests(unittest.TestCase):
 
-    def test_format(self):
-        # dict[Literal, int]
-        in_varAssignment = {Literal(name="5", 1): -1}
-        # list[Clause]
-        in_formula = [Clause(id=234, literalSet=Literal(name="5", 1))]
-        ##
-        out_varAssignment = None
-        out_formula = None
-        out = (out_varAssignment, out_formula)
-        ##
-        self.assertEqual(unitClauseElim(in_varAssignment, in_formula), out)
+    def test_no_clause(self):
+        varAssignment, formula = getInputs("test_files/noClause.txt")
+        self.assertEqual(unitClauseElim(varAssignment, formula), (varAssignment, formula))
+
+    def test_one_clause(self):
+        varAssignment, formula = getInputs("test_files/oneClause.txt")
+        new_varAssignment = varAssignment.copy()
+        new_varAssignment['1'] = 1
+        self.assertEqual(unitClauseElim(varAssignment, formula), (new_varAssignment, []))
+
+    def test_two_clause1(self):
+        varAssignment, formula = getInputs("test_files/twoClause1.txt")
+        new_varAssignment = varAssignment.copy()
+        new_varAssignment['1'] = 0
+        self.assertEqual(unitClauseElim(varAssignment, formula), (new_varAssignment, []))
+
+    def test_two_clause2(self):
+        varAssignment, formula = getInputs("test_files/twoClause2.txt")
+        new_varAssignment = varAssignment.copy()
+        new_varAssignment['1'] = 0
+        new_formula = [Clause(id=1, literalSet=[])]
+        self.assertEqual(unitClauseElim(varAssignment, formula), (new_varAssignment, new_formula))
+
+    def test_two_many_clause1(self):
+        varAssignment, formula = getInputs("test_files/manyClause1.txt")
+        new_varAssignment = varAssignment.copy()
+        new_varAssignment['1'] = 1
+        new_varAssignment['2'] = 0
+        new_formula = [Clause(id=2, literalSet=[3])]
+        self.assertEqual(unitClauseElim(varAssignment, formula), (new_varAssignment, new_formula))
 
 if __name__ == '__main__':
     unittest.main()
